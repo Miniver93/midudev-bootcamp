1508e9640c7a631b0a1552bdf0b42ccbe26f51a4
Miniver93 <josevk93@gmail.com>
2024-03-15 05:45:34 +0100

    Ejercicios 1.6 y 1.7 terminados

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,57 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+let array_votos=[]
+const App = () => {
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState()
+ 
 
-function App() {
-  const [count, setCount] = useState(0)
+  const handleOnClick=(e)=> {
+    const v_good=1
+    const v_neutral=0
+    const v_bad=-1
+    let suma_votos=0
+
+    if (e.target.textContent=="good") {
+      setGood(good + 1)
+      array_votos.push(v_good)
+    }else if(e.target.textContent=="neutral"){
+      setNeutral(neutral + 1)
+      array_votos.push(v_neutral)
+    }else if(e.target.textContent=="bad"){
+      setBad(bad + 1)
+      array_votos.push(v_bad)
+    }
+    setAll(good + neutral + bad +1)
+
+    array_votos.forEach(voto => {
+      suma_votos+=voto 
+    })
+    setAverage((suma_votos/array_votos.length).toFixed(2)) 
+
+  setPositive((good*100/all).toFixed(2))
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Give feedback</h1>
+      <button onClick={handleOnClick}>good</button>
+      <button onClick={handleOnClick}>neutral</button>
+      <button onClick={handleOnClick}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
+
+    </div>
   )
 }
 
