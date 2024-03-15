848da1b6fd7a2450ae44b700c85ad0d2d2caf445
Miniver93 <josevk93@gmail.com>
2024-03-15 04:28:56 +0100

    Comienzo ejercicios 1.6.-1.14

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,53 +1,59 @@
 /* eslint-disable react/prop-types */
 
-
 /*
 const Header=({course})=> <h1>{course}</h1>
 */
-const Header=({course})=>{
-  return <h1>{course.name}</h1>
-}
-const Content=({course})=>{
-
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+const Content = ({ course }) => {
   return (
-    <>  
-    <p>{course.parts[0].name} {course.parts[0].exercises}</p>
-    <p>{course.parts[1].name} {course.parts[1].exercises}</p>
-    <p>{course.parts[2].name} {course.parts[2].exercises}</p>
+    <>
+      <p>
+        {course.parts[0].name} {course.parts[0].exercises}
+      </p>
+      <p>
+        {course.parts[1].name} {course.parts[1].exercises}
+      </p>
+      <p>
+        {course.parts[2].name} {course.parts[2].exercises}
+      </p>
     </>
-
-  )
-}
-const Total=({course})=>{
-  const resultado=course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises
-  return <p>Number of exercises {resultado}</p>
-}
+  );
+};
+const Total = ({ course }) => {
+  const resultado =
+    course.parts[0].exercises +
+    course.parts[1].exercises +
+    course.parts[2].exercises;
+  return <p>Number of exercises {resultado}</p>;
+};
 
 const App = () => {
   const course = {
-  name: 'Half Stack application development',
-  parts: [
-  {
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: 'State of a component',
-    exercises: 14
-  }
-  ]
-}
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
   return (
     <div>
-      <Header course={course}/>
-      <Content course={course}/>
-      <Total course={course}/>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
