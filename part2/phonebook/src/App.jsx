cb29a2e432f47a490d5c0e90dbad5a67c5ed92be
Miniver93 <josevk93@gmail.com>
2024-03-16 03:24:07 +0100

    Ejercicio 2.6 terminado

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,34 +1,43 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  //Un estado con el estado inicial de un objeto con el nombre de personas
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson=(e)=>{
+    e.preventDefault()
+    const personObject={
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+  }
+
+  const handleNameInputChange=(e)=>{
+    setNewName(e.target.value)
+  }
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameInputChange}/>
+        </div>
+        <div>debug: {newName}</div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person)=>{return <li key={persons.length+=1}>{person.name}</li>})}
+      </ul>
+    </div>
   )
 }
 
