f57a5b2459d443bc7c57d695da925e5374945f11
Miniver93 <josevk93@gmail.com>
2024-03-16 01:00:04 +0100

    Trabajando de nuevo con part2 test

--- part2/test/src/App.jsx
+++ part2/test/src/App.jsx
@@ -1,25 +1,20 @@
 /* eslint-disable react/prop-types */
-import Note from "./components/note"
+import { useState } from 'react'
+import Note from './components/note'
 
-
-const App = ({notes}) => {
-  // const { notes } = props //const notes=props.notes;
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note =>
-           <li key={note.id}> {/* Siempre tengo que añadir una key */}
-            {note.content}
-            </li>
-            )}
-            {notes.map(note=>
-              <Note key={note.id} note={note}/>
-              )}
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
       </ul>
     </div>
   )
 }
 
-export default App
+export default App 
