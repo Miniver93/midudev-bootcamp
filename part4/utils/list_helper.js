8bcaffb263753d71558adfddff68033b3f2463d6
Miniver93 <128822580+Miniver93@users.noreply.github.com>
2024-03-27 04:44:42 +0100

    Ejercicio 4.5 terminado

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -9,7 +9,20 @@ const totalLikes = (blogList) => {
     return blogList.reduce((previusValue, currentValue) => previusValue + currentValue.likes, 0);
   }
 }
+
+const favoriteBlog = (blogList) => {
+  if(blogList.length === 0){
+    return 0
+  }else{
+    const mostLikes = Math.max(...blogList.map(blog => blog.likes)) //Aquí lo que estoy haciendo es buscar cual es el blog que más likes tiene
+
+    return blogList.find(blog => blog.likes === mostLikes) //Aquí recupero el blog que tenga el mayor número de likes
+     
+  }
+}
+
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
